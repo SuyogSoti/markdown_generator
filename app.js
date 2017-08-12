8ba80eb50d3a4c5a1e84bf82b679bc3e575d5832
Suyog Soti <suyog.soti@gmail.com>
2017-08-12 15:05:32 -0600

    Generated Markdown correctly

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-let fs = require('fs')
-let generator = require('./src/generator')
-let jConnect = require('./src/json')
-let showdown = require('showdown')
+const fs = require('fs')
+const generator = require('./src/generator')
+// let jConnect = require('./src/json')
+const showdown = require('showdown')
 
 // Defailt showdown configuration that is necessary
-let converter = new showdown.Converter()
+const converter = new showdown.Converter()
 converter.setFlavor('github')
 
 // get the text fromt the file
 // FIXME: Make sure that this is a commandline arguement
 let content = fs.readFileSync('./test/markdown.md').toString()
-let testJson = "/home/suyog/Documents/markdown_generator/test/db/test.json"
+// let testJson = "/home/suyog/Documents/markdown_generator/test/db/test.json"
 
-jConnect.connect(testJson)
-    .then(
-        (result) => {
-           console.log(result[0].name)
-        }
-    ).catch(e => console.log(e.toString()))
+// jConnect.connect(testJson)
+//     .then(
+//         (result) => {
+//            console.log(result[0].name)
+//         }
+//     ).catch(e => console.log(e.toString()))
 
 generator.generateJS(content)
-    .then(javascript => {
-        console.log(javascript)
-    }).catch(e => console.log(e))
+    .then(javascript => generator.generateMarkdown(javascript))
+    .then(markdown => console.log(markdown))
+
+process.on('unhandledRejection', (reason) => {
+    console.log(reason);
+});
 
 // let html = converter.makeHtml(content.toString())
 
--- src/generator.js
+++ src/generator.js
@@ -1,7 +1,5 @@
 // For the import statement
-
-
-
+const _eval = require('eval')
 
 /**
  * generateJS
@@ -15,16 +13,32 @@ exports.generateJS = (content) => new Promise((resolve, reject) => {
         content = content.split(/<script>|<\/script>/)
         for (let i in content) {
             if (i%2 == 0) {
-                let temp = content[i].split("\n")
-                for (let j in temp) {
-                    js += "console.log(" + temp[j] + ")\n"
-                }
+                content[i] = content[i].substring(0, content[i].length - 1)
+                js += "console.log(`" + content[i] + "`)\n"
             } else {
                 js += content[i]
             }
         }
         resolve(js)
+        // console.log(js)
     } else {
         reject("Error: Not a string!!")
+    }
+})
+
+/**
+ * generateMarkdown
+ * @param {string} javascript the javascript that will generate the markdown to be compiled
+ * @return {string} the markdown to be compiled
+ */
+exports.generateMarkdown = (javascript) => new Promise((resolve, reject) => {
+    // console.log(javascript)
+    let preprocess = "let Ans = '';console = {}; console.log = (anything) => Ans += anything.toString() + '\\n';"
+    let post_process =  "exports.Ans = Ans"
+    try {
+        let res = _eval(preprocess + javascript + post_process)
+        resolve(res.Ans)
+    } catch (e) {
+        reject(e)
     }
 })
